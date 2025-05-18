b55a4e2d694cfbd577355bc21b90881863bdde53
dbrito <dbrito@segware.com>
2025-05-17 21:10:23 -0300

    feat: restTemplate

--- hr-payroll/src/main/java/com/udemycourse/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/udemycourse/hrpayroll/services/PaymentService.java
@@ -1,14 +1,25 @@
 package com.udemycourse.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.udemycourse.hrpayroll.entities.Payment;
+import com.udemycourse.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String  hrWorkerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Worker worker =  restTemplate.getForObject(hrWorkerHost + "/workers/" + workerId, Worker.class);
+		return new Payment(worker.getName(),worker.getDailyIncome(), days);
 	}
 
 }
